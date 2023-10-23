6d5eb3495435fc35546ae9d19779022dd60bd2ee
JesusDBurgos <113454776+JesusDBurgos@users.noreply.github.com>
2023-10-23 08:49:04 -0500

    Cambios en graficas y vistas

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -3,7 +3,7 @@ import { ResponsiveBar } from "@nivo/bar";
 import { tokens } from "../theme";
 import { mockBarData } from "../data/mockData";
 
-const BarChart = () => {
+const BarChart = ({ isDashboard = false }) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     const data = [
@@ -196,7 +196,7 @@ const BarChart = () => {
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: "country",
+        legend: isDashboard ? undefined : "country",
         legendPosition: "middle",
         legendOffset: 32,
       }}
@@ -204,7 +204,7 @@ const BarChart = () => {
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: "food",
+        legend: isDashboard ? undefined : "food",
         legendPosition: "middle",
         legendOffset: -40,
       }}
